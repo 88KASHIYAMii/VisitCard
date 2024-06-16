596f82a7a76e3175da4c9a5078712d3d99ce3b37
ilsave <bulldoo@yandex.ru>
2024-06-16 20:18:19 +0300

    add line to check if push commit enabled

--- app/src/main/java/com/example/diplom/MainActivity.kt
+++ app/src/main/java/com/example/diplom/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
